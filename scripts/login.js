2603bc3551c51265cb6fbe3c51fba70f45456145
Leonardo Villegas Costa <leonardovillegascosta@gmail.com>
2023-09-25 21:22:00 -0300

    prueba de vinculación del back con el front para iniciar sesión

--- scripts/login.js
+++ scripts/login.js
@@ -1,10 +1,30 @@
-const passwordInput = document.getElementById('password-input');
-  const togglePassword = document.getElementById('toggle-password');
+document.addEventListener('DOMContentLoaded', function () {
+  const loginButton = document.querySelector('.desktop2-button');
 
-  togglePassword.addEventListener('click', () => {
-    if (passwordInput.type === 'password') {
-      passwordInput.type = 'text';
-    } else {
-      passwordInput.type = 'password';
-    }
+  loginButton.addEventListener('click', function () {
+      const email = document.querySelector('.desktop2-divinput3o04eu').value;
+      const password = document.querySelector('#password-input').value;
+
+      const userData = {
+          email: email,
+          password: password
+      };
+      fetch('http://127.0.0.1:5000/login', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(userData)
+      })
+      .then(response => {
+          if (response.ok) {
+              window.location.href = '../templates/main.html';
+          } else {
+              console.error('Error al iniciar sesión.');
+          }
+      })
+      .catch(error => {
+          console.error('Error de red:', error);
+      });
   });
+});
