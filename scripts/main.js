37a4e5977f20e90bd5ec0a5f024f8343f42f33d3
Leonardo Villegas Costa <leonardovillegascosta@gmail.com>
2023-09-27 15:28:05 -0300

    Modificaciones en el main.js

--- scripts/main.js
+++ scripts/main.js
@@ -34,7 +34,9 @@ document.addEventListener('DOMContentLoaded', function () {
                     const serverButton = document.createElement('button');
                     serverButton.style.backgroundImage = `url(${server.imagen_servidor})`;
                     serverButton.alt = server.nombre_servidor;
-                    serverButton.classList.add('server-button'); 
+                    serverButton.classList.add('server-button');
+                    
+                    serverButton.setAttribute('data-server-id', server.id_servidor);
 
                     serverImagesContainer.appendChild(serverButton);
                 });
@@ -47,6 +49,41 @@ document.addEventListener('DOMContentLoaded', function () {
                     const nombreServidorElement = document.querySelector('.desktop3-text12');
                     nombreServidorElement.textContent = '';
                 }
+
+                const serverButtons = document.querySelectorAll('.server-button');
+                serverButtons.forEach(button => {
+                    button.addEventListener("click", function () {
+                        const serverId = button.getAttribute('data-server-id');
+
+                        fetch(`http://127.0.0.1:5000/canal/${serverId}/`, {
+                            method: 'GET',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            }
+                        })
+                        .then(response => response.json())
+                        .then(canalesData => {
+                            const channelContainer = document.querySelector('.desktop3-channel-container');
+                            channelContainer.innerHTML = '';
+
+                            canalesData.forEach(nombre_canal => {
+                                const channelDiv = document.createElement('div');
+                                channelDiv.classList.add('desktop3-channel-item');
+
+                                const numeralImage = document.createElement('img');
+                                numeralImage.src = '../assets/hashtag.png';
+                                numeralImage.alt = '#';
+                                channelDiv.appendChild(numeralImage);
+
+                                const channelNameElement = document.createElement('span');
+                                channelNameElement.textContent = nombre_canal;
+                                channelDiv.appendChild(channelNameElement);
+
+                                channelContainer.appendChild(channelDiv);
+                            })
+                        })
+                    })
+                })
             })
             .catch(error => {
                 console.error('Error al obtener los servidores del usuario:', error);
@@ -57,3 +94,4 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
