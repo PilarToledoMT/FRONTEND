70424da091179d120d1426e90f40f653fe8be957
raffa <raffalopez@gmail.com>
2023-09-29 00:34:42 -0300

    modificar servidor.js todavia tira un error

--- scripts/modificar_servidor.js
+++ scripts/modificar_servidor.js
@@ -4,39 +4,53 @@ document.addEventListener('DOMContentLoaded', function() {
     const modificarButton = document.querySelector('.desktop9-button');
     const volverAlInicioLink = document.querySelector('.desktop9-frame3');
 
+    // Obtener los datos almacenados en localStorage
+    const nombreServidorGuardado = localStorage.getItem('nombre_servidor');
+    const imagenServidorGuardada = localStorage.getItem('imagen_servidor');
+    const idServidorGuardado = localStorage.getItem('id_servidor'); // Obtener el ID del servidor
+
+    // Llenar los campos de texto con los datos almacenados
+    nombreServidorInput.value = nombreServidorGuardado; 
+
+    // Verificar si hay una imagen almacenada y cargarla
+    if (imagenServidorGuardada) {
+        const imagen = document.createElement('img');
+        imagen.src = imagenServidorGuardada;
+        imagenServidorInput.appendChild(imagen);
+    }
+
     modificarButton.addEventListener('click', function() {
         const nombreServidor = nombreServidorInput.value;
         const imagenServidorFile = imagenServidorInput.files[0];
 
-        if (!nombreServidor) {
-            alert('Por favor, ingrese el nombre del servidor.');
-            return;
+        const requestData = {
+            nombre_servidor: nombreServidor,
+            imagen_servidor: imagenServidorFile,
+          };
+
+          
+
+        fetch(`http://127.0.0.1:5000/servidores/${idServidorGuardado}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+        })
+        .then((response) => {
+        if (response.status === 200) {
+            // Maneja una respuesta exitosa
+            alert('¡Servidor creado con éxito!');
+            nombreInput.value = '';
+            imagenInput.value = '';
+        } else {
+            // Maneja una respuesta fallida
+            alert('Error al crear el servidor.');
         }
-
-        const formData = new FormData();
-        formData.append('nombre_servidor', nombreServidor);
-        formData.append('imagen_servidor', imagenServidorFile);
-
-        fetch('/servidores', {
-            method: 'POST',
-            body: formData,
         })
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            } else {
-                throw new Error('Error en la solicitud.');
-            }
-        })
-        .then(data => {
-            if (data.message === 'Servidor creado con éxito') {
-                alert('Servidor modificado con éxito.');
-            } else {
-                alert('Error al modificar el servidor: ' + data.message);
-            }
-        })
-        .catch(error => {
-            console.error('Error en la solicitud: ' + error.message);
+        .catch((error) => {
+        // Maneja errores de red u otros errores
+        console.error('Error en la solicitud: ' + error.message);
         });
     });
 
@@ -44,4 +58,4 @@ document.addEventListener('DOMContentLoaded', function() {
         event.preventDefault();
         window.location.href = '../templates/main.html';
     });
-});
+    });
