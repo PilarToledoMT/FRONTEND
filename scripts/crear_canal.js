2fbda29801f52de1977a0d241be9490e08e60f9b
Pilar Toledo <plrtoledo@gmail.com>
2023-09-29 01:03:06 -0300

    actualizo crear canal js

--- scripts/crear_canal.js
+++ scripts/crear_canal.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", function () {
         id_servidor: id_servidor,
       };  
       
-      fetch("http://127.0.0.1:5000/canal", {
+      fetch("http://127.0.0.1:5000/canal",{
         method: "POST",
         headers: {
           "Content-Type": "application/json",
